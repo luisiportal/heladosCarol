7d4891ddecb094cfbda05f84fc5542d713f0a91b
luisiportal <luisiportal@gmail.com>
2024-08-11 22:17:57 -0400

    (arreglos nuevos domingo)

--- client/src/components/HOME/Sabores/SeccionComprar.jsx
+++ client/src/components/HOME/Sabores/SeccionComprar.jsx
@@ -9,14 +9,14 @@ const SeccionComprar = () => {
 
   return (
     <section className="w-40 pt-12 mr-4">
-      <div className="flex justify-center">
+      <div className="flex justify-center mb-2">
         <PoteHelado />
       </div>
       <h2 className="flex justify-center text-slate-700 text-xs">
-        Precio: 0.90 USD
+       
       </h2>
       <h2 className="flex justify-center text-xs text-slate-700">
-        Capacidad: 12 OZ
+       
       </h2>
      <div className="flex justify-center"> <button onClick={() => navigate("/comprar")}>
         <BTN />
