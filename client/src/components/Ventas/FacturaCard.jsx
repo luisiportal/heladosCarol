35f821255d28a264203ca23b5127c1c6f5b35a3e
luisiportal <luisiportal@gmail.com>
2024-08-22 20:59:39 -0400

    (card-venta)

--- client/src/components/Ventas/FacturaCard.jsx
+++ client/src/components/Ventas/FacturaCard.jsx
@@ -11,7 +11,6 @@ function FacturaCard({ factura, setRecargarFactura, setRecargar, recargar }) {
   const { setModalActivo, modalActivo, editando, setEditando, perfil } =
     useAuth();
 
-
   const handleEliminar = async (id) => {
     if (confirm("¿Estás a punto de eliminar una Venta ?")) {
       try {
@@ -32,49 +31,59 @@ function FacturaCard({ factura, setRecargarFactura, setRecargar, recargar }) {
 
   return (
     <div
-      className={`mx-4 md:mx-1 my-1 bg-neutral-200 shadow rounded overflow-hidden p-2 max-w-md`}
+      className={`mx-4 md:mx-1 my-1 bg-neutral-200 shadow rounded overflow-hidden max-w-md`}
     >
-      <div className="px-3 text-left text-slate-700 font-semibold w-full h-full align-middle flex flex-col">
-        <p>Factura : {factura.id}</p>
-
-        {ventas.map((sabor) => (
-          <div className="flex justify-between" key={sabor.id_venta}>
-            <div className="flex">
-              <h2> {sabor.cantidad}x </h2>
-              <h2>{sabor.sabore?.nombre_sabor ?? sabor.nombre_sabor}</h2>
-            </div>
-
-            <h2>
-              {sabor.precio_total_sabor ?? sabor.precio_venta * sabor.cantidad}{" "}
-              USD
-            </h2>
-          </div>
-        ))}
-
-        <div className="text-right mt-5 flex-grow flex flex-col">
-          <p>Total {factura.total_venta} USD</p>
-          <p>Detalles:</p>
-          <h2>Ordenante: {factura.entrega.ordenante} </h2>
-          <h2>Contacto:  {factura.entrega.contacto_ordenante}</h2>
-
-           
-            {factura.entrega.contacto_ordenante}
-            {factura.entrega.beneficiario}
-            {factura.entrega.tel_beneficiario}
-            {factura.entrega.calle}
-            {factura.entrega.numero}
-            {factura.entrega.calle1}
-            {factura.entrega.calle2}
-            {factura.entrega.reparto}
-            {factura.entrega.p_referencia}
-        
-
+      <div className="text-left text-slate-700 font-semibold w-full h-full align-middle flex flex-col">
+        <div className="flex justify-between font-extralight  text-sm m-2">
+          {" "}
+          <p>Factura : {factura.id}</p>
           <p>
             {new Date(factura.creado).toLocaleString("es-ES")}{" "}
             <button onClick={() => setEditando(factura.id)}>
               <EditSVG />
             </button>
           </p>
+        </div>
+        <div className="bg-fresa rounded-2xl text-slate-900 font-extralight p-2 px-6 shadow-xl drop-shadow-xl mx-4">
+          {ventas.map((sabor) => (
+            <div className="flex justify-between" key={sabor.id_venta}>
+              <div className="flex text-xl">
+                <h2>
+                  {" "}
+                  {sabor.cantidad}x{" "}
+                  {sabor.sabore?.nombre_sabor ?? sabor.nombre_sabor}
+                </h2>
+              </div>
+
+              <h2>
+                {sabor.precio_total_sabor ??
+                  sabor.precio_venta * sabor.cantidad}{" "}
+                USD
+              </h2>
+            </div>
+          ))}
+          <p className="text-right">Total {factura.total_venta} USD</p>
+        </div>
+
+        <div className="flex-grow flex flex-col space-y-2 font-light  p-4">
+          <p>Entregar a:</p>
+          <div className="flex flex-col gap-4 ml-4">
+            <div>
+              <h2>
+                <h2>Beneficiario : {factura.entrega.beneficiario} </h2>
+                Dirección : Calle {factura.entrega.calle} #{" "}
+                {factura.entrega.numero} entre {factura.entrega.calle1} y{" "}
+                {factura.entrega.calle2} Reparto {factura.entrega.reparto}{" "}
+              </h2>
+              <h2>Referencia : {factura.entrega.p_referencia} </h2>
+              <h2>Teléfono : {factura.entrega.tel_beneficiario} </h2>
+            </div>
+
+            <div>
+              <h2>Enviado por: {factura.entrega.ordenante} </h2>
+              <h2>Contacto: {factura.entrega.contacto_ordenante}</h2>
+            </div>
+          </div>
         </div>
 
         {perfil.privilegio == "Administrador" ? (
