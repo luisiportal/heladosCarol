2528da0dfe8e1a14526ab7304273be3289cdc1f0
luisiportal <luisiportal@gmail.com>
2024-08-23 11:44:00 -0400

    (arreglado 0.5)

--- client/src/components/Carusel/Carusel.jsx
+++ client/src/components/Carusel/Carusel.jsx
@@ -5,12 +5,16 @@ import "react-image-gallery/styles/css/image-gallery.css";
 const Carusel = () => {
   const images = [
     {
-      original: "../images/cdn-3.expansion.webp",
-      thumbnail: "../images/cdn-3.expansion.webp",
+      original: "../images/carusel/saboresSolo.jpg",
+      thumbnail: "../images/carusel/saboresSolo.jpg",
     },
     {
       original: "../images/carusel/chocolateBoleadora.jpg",
       thumbnail: "../images/carusel/chocolateBoleadora.jpg",
+    },
+    {
+      original: "../images/carusel/2.jpg",
+      thumbnail: "../images/carusel/2.jpg",
     },
     {
       original: "../images/carusel/imagen1.jpg",
@@ -25,10 +29,7 @@ const Carusel = () => {
       original: "../images/carusel/pote3.jpg",
       thumbnail: "../images/carusel/pote3.jpg",
     },
-    {
-      original: "../images/carusel/saboresSolo.jpg",
-      thumbnail: "../images/carusel/saboresSolo.jpg",
-    },
+
   ];
   return (
     <div className="w-full flex">
