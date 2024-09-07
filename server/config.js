a844b6708ea56f21d76549ccd44f400e1be0d72f
luisiportal <luisiportal@gmail.com>
2024-09-06 22:17:01 -0400

    (confirmada factura)

--- server/config.js
+++ server/config.js
@@ -10,5 +10,5 @@ export const HTTPONLY = process.env.HTTPONLY || false
 export const SAMESITE = process.env.SAMESITE || "none"
 export const DOMAIN = process.env.DOMAIN || ""
 export const TBOT = process.env.TBOT || "7534147770:AAFYQOZgFK4bPlSk62ajqD12B3bmaficmtU" 
-export const SECRET_PAY_KEY = process.env.SECRET_PAY_KEY || "sk_test_51PuCdKRpMkMjmn3C9UXUOAoGwvJ0TbTYUqWFXJ4Mo92KNl00eTY2mDN38juBNlFZAmXHnNhIA1RaJPrzJ4ey6sEA005nstIipe"
+export const SECRET_PAY_KEY = process.env.SECRET_PAY_KEY
 
