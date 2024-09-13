566041f71c2430ca21c5e29165b49935a0ceb8dd
luisiportal <luisiportal@gmail.com>
2024-09-12 21:16:53 -0400

    (robot text)

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -4,17 +4,18 @@ import { Navigate, Outlet } from "react-router-dom";
 import { cargarPerfilRequest } from "../api/login.api";
 
 const ProtectedRoutes = () => {
-  const { loading, user, isAuthenticated,setPerfil } = useAuth();
+  const { loading, user, isAuthenticated, setPerfil } = useAuth();
 
   if (loading) return <h1>Cargando</h1>;
   useEffect(() => {
     const cargarPerfil = async () => {
-    const {data}= await cargarPerfilRequest(user.id);
-    setPerfil(data)
+      const { data } = await cargarPerfilRequest(user.id);
+      setPerfil(data);
     };
+    cargarPerfil();
   }, []);
 
-  if (!isAuthenticated) return <Navigate to="/trabajador/login" replace />;
+  if (!isAuthenticated) return <Navigate to="/" replace />;
 
   return <Outlet></Outlet>;
 };
