7a1bf61455c70fae65e8cc3265bd798effc38d34
luisiportal <luisiportal@gmail.com>
2024-11-07 23:48:46 -0500

    (tropipay verifcarjsonencode)

--- server/controllers/Tropipay.controllers.js
+++ server/controllers/Tropipay.controllers.js
@@ -26,7 +26,7 @@ export const createPago = async (req, res) => {
   const { description, totalCobrar, fechaFactura, reference } = req.body;
   const token = await getAccessToken();
 
-  const notificacionURL = "https://api.heladoscarol.com/";
+  const notificacionURL = `${BACKEND_URL}/verificarpago`;
   const options = {
     method: "POST",
     headers: {
