e42f738fcca44cb6f557bbeba28577ae823f684f
luisiportal <luisiportal@gmail.com>
2024-11-09 23:40:35 -0500

    (add tinas)

--- client/src/components/HOME/TinaCardElement.jsx
+++ client/src/components/HOME/TinaCardElement.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TinaCardElement = ({ tina }) => {
   return (
-    <div style={{ backgroundColor: "#" + tina.color }} className="flex justify-center items-center font-irish rounded-lg p-2 w-24 h-16">{tina.nombre_sabor}</div>
+    <div style={{ backgroundColor: "#" + tina.color }} className="flex justify-center items-center font-irish text-md text-center rounded-lg p-2 w-24 h-16">{tina.nombre_sabor}</div>
   );
 };
 
