935202ad89f48ff747a9da7fe04882587f36807b
luisiportal <luisiportal@gmail.com>
2024-11-24 21:15:10 -0500

    (serviceworker)

--- client/src/api/notifications.js
+++ client/src/api/notifications.js
@@ -6,8 +6,10 @@ export const suscribeRequest = async () => {
   if ("serviceWorker" in navigator) {
     try {
       // Verifica si el navegador soporta Service Workers
-      const registration = await navigator.serviceWorker.register(`${import.meta.env.VITE_BACKEND_URL}/sw.js`, {scope:("www.heladoscarol.com/")});
-      console.log('Service Worker registrado con éxito:', registration);
+      const registration = await navigator.serviceWorker.register(`/sw.js`, {
+        scope: "/",
+      });
+      console.log("Service Worker registrado con éxito:", registration);
 
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
@@ -20,6 +22,6 @@ export const suscribeRequest = async () => {
       console.error(`Falló el registro con el error: ${error}`);
     }
   } else {
-    console.log('Service Worker no es soportado por este navegador.');
+    console.log("Service Worker no es soportado por este navegador.");
   }
 };
