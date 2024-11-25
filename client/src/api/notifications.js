b9f9207d9309510b46e66d1e2674be50c60a7d9b
luisiportal <luisiportal@gmail.com>
2024-11-24 22:11:35 -0500

    (serviceworker)

--- client/src/api/notifications.js
+++ client/src/api/notifications.js
@@ -1,22 +1,39 @@
 import axios from "./axios.js";
 
-const PUBLICWpKey = import.meta.env.WEBPUSH_PUBLIC;
+const PUBLICWpKey = import.meta.env.VITE_WEBPUSH_PUBLIC;
 
 export const suscribeRequest = async () => {
   if ("serviceWorker" in navigator) {
     try {
-      // Verifica si el navegador soporta Service Workers
-      const registration = await navigator.serviceWorker.register(`/sw.js`, {
+      // Solicitar permisos de notificaciones dentro de un evento generado por el usuario
+      const permission = await Notification.requestPermission();
+      if (permission !== "granted") {
+        throw new Error("Permiso para notificaciones denegado");
+      }
+
+      // Registrar el Service Worker
+      const registration = await navigator.serviceWorker.register("/sw.js", {
         scope: "/",
       });
       console.log("Service Worker registrado con éxito:", registration);
 
+      // Suscribirse a las notificaciones push
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: PUBLICWpKey,
       });
 
-      await axios.post(`/suscription`, JSON.stringify(subscription));
+      // Convertir la suscripción a JSON
+      const dataJson = JSON.stringify(subscription);
+      console.log(dataJson);
+
+      // Enviar la suscripción al servidor como JSON
+      await axios.post(`/suscription`, dataJson, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
       console.log("Solicitud de suscripción realizada con éxito");
     } catch (error) {
       console.error(`Falló el registro con el error: ${error}`);
--- server/controllers/suscriptions.controller.js
+++ server/controllers/suscriptions.controller.js
@@ -1,7 +1,22 @@
-import { webPushTool } from "./webPush.js"
+import webPush from "web-push";
 
 export const suscribe = async (req, res) => {
-    console.log(req.body);
+  const pushSubscription = req.body;
 
-    res.status(200).json();
-}
+  // Responde inmediatamente al cliente para confirmar la recepción
+  res.status(200).json("Subscription received");
+
+  // Define el payload de la notificación
+  const payload = JSON.stringify({
+    title: "Mi Notificación",
+    message: "Este es el mensaje",
+  });
+
+  try {
+    // Envía la notificación push
+    await webPush.sendNotification(pushSubscription, payload);
+    console.log("Notificación enviada con éxito");
+  } catch (error) {
+    console.error("Error al enviar la notificación:", error);
+  }
+};
