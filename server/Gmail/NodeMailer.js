d4427e3378929ff83dcc4690fcf11d9342d6dc70
luisiportal <luisiportal@gmail.com>
2024-12-01 21:00:45 -0500

    (probando plantilla coreeo)

--- server/Gmail/NodeMailer.js
+++ server/Gmail/NodeMailer.js
@@ -5,7 +5,7 @@ import path from "path";
 import Handlebars from "handlebars";
 
 // Definir el helper 'eq'
-Handlebars.registerHelper('eq', function (a, b) {
+Handlebars.registerHelper("eq", function (a, b) {
   return a === b;
 });
 
@@ -26,8 +26,8 @@ export const EnviarCorreo = async (to, subject, text, html) => {
       partialsDir: path.resolve("./views/"), // Ajustar ruta según tu estructura
       defaultLayout: false,
       helpers: {
-        eq: (a, b) => a === b // Agregar el helper 'eq' aquí también
-      }
+        eq: (a, b) => a === b, // Agregar el helper 'eq' aquí también
+      },
     },
     viewPath: path.resolve("./views/"), // Ajustar ruta según tu estructura
     extName: ".hbs",
@@ -39,32 +39,31 @@ export const EnviarCorreo = async (to, subject, text, html) => {
     from: '"Helados Carol" <heladoscarol@gmail.com>',
     to: to,
     subject: subject,
-    text: text,
     template: "email",
     context: {
       factura: {
-        id: '12345',
-        creado: '2024-11-29',
-        pasarela: 'USD'
+        id: "12345",
+        creado: "2024-11-29",
+        pasarela: "USD",
       },
       productos: [
-        { nombre_sabor: 'Chocolate', cantidad: 2 },
-        { nombre_sabor: 'Vainilla', cantidad: 1 }
+        { nombre_sabor: "Chocolate", cantidad: 2 },
+        { nombre_sabor: "Vainilla", cantidad: 1 },
       ],
       entrega: {
-        beneficiario: 'Juan Pérez',
-        calle: 'Primera',
-        numero: '123',
-        calle1: 'Segunda',
-        reparto: 'Centro',
-        p_referencia: 'Frente al parque',
-        tel_beneficiario: '555-123456',
-        ordenante: 'Carlos López',
-        contacto_ordenante: 'carlos@example.com',
-        observaciones: 'Entregar antes de las 5 PM'
+        beneficiario: "Juan Pérez",
+        calle: "Primera",
+        numero: "123",
+        calle1: "Segunda",
+        reparto: "Centro",
+        p_referencia: "Frente al parque",
+        tel_beneficiario: "555-123456",
+        ordenante: "Carlos López",
+        contacto_ordenante: "carlos@example.com",
+        observaciones: "Entregar antes de las 5 PM",
       },
-      grandTotalCobrar: '150.00'
-    }
+      grandTotalCobrar: "150.00",
+    },
   };
 
   // Enviar el correo
