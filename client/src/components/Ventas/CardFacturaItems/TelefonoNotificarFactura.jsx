58a9903a3fa55db642f014d33084f9679f31fb2b
luisiportal <luisiportal@gmail.com>
2024-12-05 23:48:01 -0500

    (social y noti wasapp)

--- client/src/components/Ventas/CardFacturaItems/TelefonoNotificarFactura.jsx
+++ client/src/components/Ventas/CardFacturaItems/TelefonoNotificarFactura.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const TelefonoNotificarFactura = ({ numero, ventas, grandTotal, moneda,persona }) => {
+const TelefonoNotificarFactura = ({
+  numero,
+  ventas,
+  grandTotal,
+  moneda,
+  persona,
+}) => {
   const items = ventas.map(
     (item) => `${item.cantidad} ${item.sabore.nombre_sabor} `
   );
@@ -8,13 +14,23 @@ const TelefonoNotificarFactura = ({ numero, ventas, grandTotal, moneda,persona }
   const texto = `Hola ${persona}, hemos recibido su factura de ${items} ${
     moneda === "CUP" ? `con un total de ${grandTotal} CUP` : ""
   } . La cual ya está siendo procesada. Helados Carol | www.heladoscarol.com`;
-  
+
+  function agregarPrefijo(numero) {
+   
+    if (!numero.startsWith("53") && !numero.startsWith("+53")) {
+       numero = "53" + numero;
+    }
+    return numero;
+  }
+
+  const resultado = agregarPrefijo(numero);
+ 
 
   return (
     <a
       className="flex font-semibold"
       target="_blank"
-      href={`https://wa.me/${numero}?text=${texto}`}
+      href={`https://wa.me/${resultado}?text=${texto}`}
     >
       <img className="w-8 h-8" src="/images/was100.png" alt="WhatsAPP" />
       {numero}
