0362091e338a731a7cda1011c60e0c0cbb0bf7a1
luisiportal <luisiportal@gmail.com>
2025-06-10 11:43:06 -0400

    (estadistica reserva)

--- client/src/components/Reservas/EstadisticaReserva.jsx
+++ client/src/components/Reservas/EstadisticaReserva.jsx
@@ -11,25 +11,33 @@ const EstadisticaReserva = ({ facturas }) => {
     return ventas.filter((item) => item.sabore.nombre_sabor === nombre).length;
   };
 
+  const porDia = (dia) => {
+    return facturas.filter((item) => item.fechaEntrega === dia).length;
+  };
+
   const cantCakeMediano = cantidadCombos("Combo 2");
   const cantCakeChiquito =
     cantidadCombos("Combo 1") + cantidadCombos("Combo 3");
   const cantPotes = cantidadCombos("Combo 3") * 5;
   const cantTinas = cantidadCombos("Combo 1") + cantidadCombos("Combo 2");
 
+  const sabado = porDia("sabado");
+  const domingo = porDia("domingo");
 
   return (
     <div className="bg-neutral-200 pt-10">
-    
       <div className="flex justify-center gap-2">
         {" "}
         <ItemStadistica nombre={"Cakes Chiquitos"} valor={cantCakeChiquito} />
         <ItemStadistica nombre={"Cakes Medianos"} valor={cantCakeMediano} />
       </div>
-      <div className="flex justify-center gap-14">
+      <div className="flex justify-center">
         {" "}
         <ItemStadistica nombre={"Tinas"} valor={cantTinas} />
         <ItemStadistica nombre={"Potes"} valor={cantPotes} />
+        <ItemStadistica nombre={"Sábado"} valor={sabado} color1="bg-yellow-400" color2="bg-yellow-200 text-neutral-900" />
+        <ItemStadistica nombre={"Domingo"} valor={domingo} color1="bg-yellow-400" color2="bg-yellow-200 text-neutral-900" />
+
       </div>
     </div>
   );
--- client/src/components/Reservas/ItemStadistica.jsx
+++ client/src/components/Reservas/ItemStadistica.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-const ItemStadistica = ({ nombre, valor }) => {
+const ItemStadistica = ({
+  nombre,
+  valor,
+  color1 = "bg-fresa",
+  color2 = "bg-fuchsia-200",
+}) => {
   return (
     <div className="flex flex-col justify-center items-center mt-2 p-5">
-      <div className="bg-fuchsia-200 rounded-full w-14 h-14 flex justify-center items-center text-white">
-        <h2 className="bg-fresa rounded-full p-2 w-8 h-8 flex justify-center items-center font-semibold">
+      <div
+        className={` ${color2} rounded-full w-14 h-14 flex justify-center items-center text-white`}
+      >
+        <h2
+          className={`${color1} rounded-full p-2 w-8 h-8 flex justify-center items-center font-semibold`}
+        >
           {valor}
         </h2>
       </div>
