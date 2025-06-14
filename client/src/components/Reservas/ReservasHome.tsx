3c7daf4ff5a1fb77dccb7cc8fe9cc1a92945a210
luisiportal <luisiportal@gmail.com>
2025-06-14 18:28:55 -0400

    (quitar reservar)

--- client/src/components/Reservas/ReservasHome.tsx
+++ client/src/components/Reservas/ReservasHome.tsx
@@ -5,15 +5,13 @@ import { Link } from "react-router-dom";
 const ReservasHome = () => {
   return (
     <div className="m-2">
-
-      <Link to={"/reservar"} className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <img
           className="rounded-lg mb-2"
           src="images/padres.jpg"
           alt="Feliciades Papá"
         />
-        <BTN color={"#f9a217"} texto="Reservar" />
-      </Link>
+      </div>
     </div>
   );
 };
