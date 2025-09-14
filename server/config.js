7996afba47992584860339b63ab25f65ab2d454a
luisiportal <luisiportal@gmail.com>
2025-09-13 23:23:01 -0400

    (pagos zelle 2)

---
+++ server/config.js
@@ -0,0 +1,31 @@
+import path from "path";
+
+export const PUERTO = process.env.PORT || 5000;
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_PORT = process.env.DB_PORT || "5444";
+export const DB_DATABASE = process.env.DB_DATABASE || "heladosCarol";
+export const DB_USER = process.env.DB_USER || "postgres";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "otravuelta";
+export const SECURE = process.env.SECURE || true;
+export const HTTPONLY = process.env.HTTPONLY || true;
+export const SAMESITE = process.env.SAMESITE || "none";
+export const DOMAIN = process.env.DOMAIN || "";
+export const TBOT = process.env.TBOT;
+export const SECRET_PAY_KEY = process.env.SECRET_PAY_KEY;
+export const GMAIL_KEY = process.env.GMAIL_KEY;
+export const CLIENT_ID = process.env.CLIENT_ID;
+export const CLIENT_SECRET = process.env.CLIENT_SECRET;
+export const WEBPUSH_PRIVATE = process.env.WEBPUSH_PRIVATE;
+export const WEBPUSH_PUBLIC = process.env.WEBPUSH_PUBLIC;
+export const CONSUMER_KEY = "";
+export const CONSUMER_SECRET = "";
+
+export const GMAIL_CLIENT_ID = process.env.GMAIL_CLIENT_ID ;
+export const GMAIL_CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET ;
+export const GMAIL_REDIRECT_URL = process.env.GMAIL_REDIRECT_URL ;
+
+export const TOKEN_PATH = path.resolve(
+  process.env.TOKEN_PATH || "/token.json"
+);
