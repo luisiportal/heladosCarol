811d889e7f7c5fc13d2d3b793b9347daddfe5257
luisiportal <luisiportal@gmail.com>
2025-12-14 09:15:47 -0500

    (facebook)

--- client/src/components/redesSociales/FacebookSeguir.tsx
+++ client/src/components/redesSociales/FacebookSeguir.tsx
@@ -7,10 +7,10 @@ const FacebookSeguir = () => {
         <TituloModulo titulo="Síguenos en Facebook" />
       </div>
 
-      <div className="w-full flex justify-center">
+      <div className="w-full flex justify-center rounded-xl">
         <iframe
         title="Facebook Helados Carol"
-          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fheladoscarolhlg&tabs=timeline&width=340&height=400&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fheladoscarolhlg&tabs=timeline&width=400&height=400&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
           width="375"
           height="400"
           allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
